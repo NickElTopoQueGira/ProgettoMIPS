734b79df422d1eb3c92575ed5a5d34797450251c
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-07-11 01:03:15 +0200

    Χαλεπὰ τὰ καλά

--- progetto.asm
+++ progetto.asm
@@ -28,6 +28,7 @@ RECORD:         .space 32   # definisco lo spazio di 'RECIRD'
 # contatori
 cont_reset:     .word 0     # contatore per il reset
 cont_allarm:    .word 0     # contatore per la sirena
+cont_sensor:    .word 0     # contatore sensori attivi
 
 .text
 .globl main
@@ -44,14 +45,14 @@ main:
     # contatori
     la      $s4, cont_reset         # carico l'indirizzo del contatote del reset nel registro $s4
     la      $s5, cont_allarm        # carico l'indirizzo del contatore del rest dell'allarme nel registro $s5 
-
+    la      $s6, cont_sensor        # carico l'indirizzo del contatore dei sensori attivi
 
     # ciclo di lettura dell'area di memoria 'TEMPERATURE'    
     leggi_temperature:
         move     	$t0, $zero              # $t0 contatore 
         move        $t1, $s2                # copia temporanea di $s2
         ciclo_di_lettura:
-            bge         $t0, 16, leggi_temperature              # controllo se ho letto tutto lo spazio di memoria
+            bge         $t0, 16, main                           # controllo se ho letto tutto lo spazio di memoria
                                                                 # 16 = 64Byte / 4Byte 
                                                                 # quando arrivo al limite massimo, rincomincio da 0
             # $t2 -> senosre (1 word = (2Byte + 2Byte)) 
@@ -65,15 +66,58 @@ main:
             andi        $t4, $t2, 0xFFF     # ottengo il valore del sensore (16 bit a destra)
 
             # faccio i controlli
+                # se la temperatura e' minore di 40gradi
+                ble     $t4, 0x28, aggiorna_successivo        # se la temperatura e' <= 40 gradi
+                                                              # vado al successivo
+                # altrimenti:
+                # la temperatura non e' minore di 40
+                jal     temp_maggiore                         # la temperatura e' > 40 gradi
 
             # aggiornamento del contatore e calcolo dell'indirizzo successivo da leggere
-            addi        $t0, 1              # incremento il contatore di 1
-            addi        $t1, $t1, 4         # vado alla prossima word in memoria
-            j           ciclo_di_lettura    # ritorno al ciclo di lettura
+            aggiorna_successivo:
+                addi        $t0, 1              # incremento il contatore di 1
+                addi        $t1, $t1, 4         # vado alla prossima word in memoria
+                j           ciclo_di_lettura    # ritorno al ciclo di lettura
 
 
+# TODO: da rivedere la logica
 
+# -------------- TEMPERATURA MAGGIORE DI 40 GRADI --------------
+temp_maggiore:
+    # Aggiungo il numero del sensore all'interno di RECORD
+    sll     $t5, $t0, 1                 # calcolo dell'offset
+                                        # $t0 * 2Byte
+    add     $t6, $s3, $t5               # indirizzo di dove mettere il valore
+                                        # nell'area di memoria RECORD
+    sh      $t3, 0($t6)                 # salvo l'id del sesnore
 
+    # Se la temperatura e' minore di 60 gradi
+    blt     $t4, 0x3C, temp_max_exit    # se la temperatura e' < 60gradi
+    
+    # temperatura >= 60 gradi
+    jal     temp_sessanta
+
+temp_max_exit:
+    jr      $ra                         # ritorno al chiamante
+
+# -------------- TEMPERATURA MAGGIORE DI 60 GRADI --------------
+temp_sessanta:
+    # Attivazione della sirena
+    jal     attiva_sirena               # attivazione della sirena
+    jal     agg_cont_sensor             # aggiorno il contatore sensori attivi
+
+    # attivazione dell'acqua
+    lw      $t7, $0($s6)                # leggo il valore aggiornato del contatore
+                                        # dei sensori attivi
+    bgt     $t7, 0x2, sirena            # controllo delle condizioni per attivare la sirena
+
+    # attivazione acqua e chiamata ai VVFF
+    
+
+# -------------- CONDIZIONI X ATTIVAZIONE DELLA SIRENA --------------
+sirena:
+    syscall
+    
 # -------------- AGGIORNAMENTO CONTATORI --------------
 
 # aggiornamento del contatore di rest
@@ -86,9 +130,10 @@ agg_cont_reset:
 reset_cont_reset:
     lw      $t9, 0($s4)             # carico nel registro $t9 il valore di $s4
     move    $t9, $zero              # azzero
-    sw      $t9, 0($s4)                # aggiorno $s4 con il nuovo valore
+    sw      $t9, 0($s4)             # aggiorno $s4 con il nuovo valore
     jr      $ra                     # ritorno al chiamante
 
+# aggiornamento contatore per la sirena
 agg_cont_allarm:
     lw      $t9, 0($s5)             # carico nel registro $t9 il valore di $s5
     addi    $t9, $t9, 1             # incremento il contatore di 1
@@ -100,6 +145,20 @@ reset_cont_allarm:
     move    $t9, $zero              # azzero
     sw      $t9, 0($s5)             # aggiorno $s5 con il nuovo valore
     jr      $ra                     # ritorno al chiamante
+
+# aggiornamento contatore sensori attivi
+agg_cont_sensor:
+    lw      $t9, 0($s6)             # carico nel registro $t9 il valore di $s6
+    addi    $t9, $t9, 1             # incremento il contatore di 1
+    sw      $t9, 0($s6)             # aggiorno $s6 con il nuovo valore
+    jr      $ra                     # ritorno al chiamante
+
+reset_cont_sensor:
+    lw      $t9, 0($s6)             # carico nel registro $t9 il valore di $s6
+    move    $t9, $zero              # azzero
+    sw      $t9, 0($s6)             # aggiorno $s6 con il nuovo valore
+    jr      $ra                     # ritorno al chiamante
+
 
 # -------------- COMMAND --------------
 
